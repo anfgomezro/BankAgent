a33be994012c720e7492c6a673f83856d396a9ea
Felipe Gomez <fgomez@endava.com>
2018-02-05 17:33:03 -0500

    added interfaz for concole

--- src/Dispatcher.java
+++ src/Dispatcher.java
@@ -6,16 +6,18 @@ public class Dispatcher {
 
     private Bank bank;
     private ExecutorService executor;
+    private int numberClients;
 
 
-    public Dispatcher(Bank bank, ExecutorService executor) {
+    public Dispatcher(Bank bank, ExecutorService executor,int numberClients) {
         this.bank = bank;
         this.executor = executor;
+        this.numberClients = numberClients;
     }
 
     public void attend(){
         int clientId = 0;
-        while(clientId < 10){
+        while(clientId < numberClients){
             for(Employee employees : bank.getEmployees()){
                 if(employees.isStatus()){
                     Client client = new Client(employees,clientId);
